6400f99b3fb40d247adc07d104babcbc31c8d5c4
rxnew <yujun04072010@163.com>
2017-11-24 18:00:35 +0800

    百度地图

--- grwz_dao/src/main/java/net/test/dao/htgl/tree/TreeDao.java
+++ grwz_dao/src/main/java/net/test/dao/htgl/tree/TreeDao.java
@@ -3,11 +3,9 @@ package net.test.dao.htgl.tree;
 import net.test.dao.BaseDao;
 import net.test.daomain.main.User;
 import org.springframework.stereotype.Repository;
-import paginate.FastPagination;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+
 
 /**
  * Created by yucheng on 2017/9/10.
@@ -16,11 +14,17 @@ import java.util.Map;
 public class TreeDao extends BaseDao<User> {
 
     public List getMenu(Integer id) {
-        List a=new ArrayList();
-        if(id==null){id=new Integer(0);}
+        List a = new ArrayList();
+        StringBuffer sql;
+        if (id == null) {
+            id = new Integer(0);
+            sql = new StringBuffer("SELECT 'true' \"checked\" ,U.ID \"id\",U.MC \"name\",U.SJMENU \"pid\" ," +
+                    "DECODE((SELECT COUNT(1) FROM GRWZ_MENU WHERE SJMENU=U.ID AND TYPE<>3 ),0,'FALSE','TRUE') \"isParent\" FROM GRWZ_MENU U WHERE ID=?");
+        } else {
+            sql = new StringBuffer("SELECT  U.ID \"id\",U.MC \"name\",U.SJMENU \"pid\" ," +
+                    "DECODE((SELECT COUNT(1) FROM GRWZ_MENU WHERE SJMENU=U.ID AND TYPE<>3 ),0,'FALSE','TRUE') \"isParent\" FROM GRWZ_MENU U WHERE SJMENU=? AND TYPE<>3 ");
+        }
         a.add(id);
-        StringBuffer sql=new StringBuffer("SELECT  U.ID \"id\",U.MC \"name\",U.SJMENU \"pid\" ," +
-                "DECODE((SELECT COUNT(1) FROM GRWZ_MENU WHERE SJMENU=U.ID AND TYPE<>3 ),0,'FALSE','TRUE') \"isParent\" FROM GRWZ_MENU U WHERE SJMENU=? AND TYPE<>3 ");
-        return getJdbcTemplate().queryForList(sql.toString(),a.toArray());
+        return getJdbcTemplate().queryForList(sql.toString(), a.toArray());
     }
 }
